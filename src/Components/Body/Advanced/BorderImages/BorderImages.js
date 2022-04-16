93ebbd65d4c9cf7b7956de2b154343b5d3ca4d15
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-04-16 20:43:34 +0800

    border image

--- src/Components/Body/Advanced/BorderImages/BorderImages.js
+++ src/Components/Body/Advanced/BorderImages/BorderImages.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from '../../../Layout/Layout';
+import styles from './BorderImages.module.css';
 
 const BorderImages = () => {
+
+    const [ borderImages, setBorderImages ] = useState(false);
+
     return (
         <Layout>
             <div className='name'>
                 <h2>Border Images</h2>
             </div>
-            
+            <section className="section">
+                {/* border width */}
+                <h2>Border Width</h2>
+                <p>The width can be set as a specific size (in px, pt, cm, em, etc) or by using one of the three pre-defined values: thin, medium, or thick.</p>
+                
+                <div className="codeRunDiv">
+                        <div className={styles.firstSection}></div>
+
+                            <br /><br />
+                        {
+                        borderImages ? <button className="seeCodeButton" onClick={() => setBorderImages(!borderImages)}>hide output</button> : <button className="seeCodeButton" onClick={() => setBorderImages(!borderImages)}>output</button>
+                        }
+                    </div>
+                {
+                    borderImages && <div className="codeDiv">
+                    <code>
+                        <span className='seclector'>.firstSection</span> &#123; <br />
+                        &nbsp;&nbsp;<span className='cssName'>border</span>: <span className='cssTextvalue'>70px solid black</span>;<br />
+                        &nbsp;&nbsp;<span className='cssName'>height</span>: <span className='cssTextvalue'>400px</span>;<br />
+                        &nbsp;&nbsp;<span className='cssName'>width</span>: <span className='cssTextvalue'>400px</span>;<br />
+                        &nbsp;&nbsp;<span className='cssName'>border-image</span>: <span className='cssTextvalue'>url('borderImage.png') 33% round</span>;<br />
+                        &#125;<br />
+
+                        <br /><br />
+
+                        &lt;div class='firstSection' &gt; &lt;/div&gt;
+                    </code>
+                </div>
+                }
+            </section>
         </Layout>
     );
 };
